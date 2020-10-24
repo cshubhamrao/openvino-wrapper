070ac80aa1f7a055f18e36e5e1029bcdb04e27ec
Shubham Rao <cshubhamrao@gmail.com>
2020-10-24 22:34:02 +0530

    test version print

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,18 @@
 from .tools.benchmark.benchmark import Benchmark
 
-config = {}
+args = {
+    'device': 'CPU',
+    'number_infer_requests': 8,  # TODO: replace with auto infer?
+    'number_iterations': 1,  # TODO: refactor out of Benchmark class
+    'duration_seconds': None,  # TODO: refactor out of Benchmark class
+    'api_type': 'async',
+    'path_to_model': ''
+}
 
-benchmark = Benchmark(args.target_device, args.number_infer_requests,
-                              args.number_iterations, args.time, args.api_type)
+config = {args['device']: {
+    'CPU_THROUGHPUT_STREAMS': 'CPU_THROUGHPUT_AUTO'
+}}
+device = args['device']
+if __name__ == '__main__':
+    benchmark = Benchmark(**args)
+    print(benchmark.get_version_info())
